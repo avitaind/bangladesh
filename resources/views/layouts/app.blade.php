872098ed3f36bc8481bce5ece432c0d61a1ba9bc
AVITA India <avitaind@gmail.com>
2019-11-04 12:25:41 +0530

    BD country added to app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 @php
+$country='bd';
     $xml_lang = ($lang == 'en') ? 'en' : 'zh-hk';
 
     $keyword = metaKeywordByCountryAndLanguage( $country, $lang);
