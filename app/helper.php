8d2c8fa033619e52b9130964babf263ff1600b94
AVITA India <avitaind@gmail.com>
2019-11-04 12:09:27 +0530

    BD Slug Added in Helper File

--- app/helper.php
+++ app/helper.php
@@ -18,26 +18,8 @@ function enabled_countries(  ) {
 function supported_language( $country ) {
 
     switch ( $country ) {
-        case 'hk':
+        case 'bd':
             return ['en', 'tc'];
-        case 'sg':
-            return ['en'];
-        case 'tw':
-            return ['tc'];
-        case 'cn':
-            return ['sc'];
-        case 'ph':
-            return ['en'];
-        case 'th':
-            return ['en', 'th'];
-        case 'vn':
-            return ['en', /* 'vn' */];
-        case 'id':
-            return ['en', /* 'id' */];
-        case 'in':
-            return ['en'];
-        default:
-            return ['en'];
     }
 
 }
@@ -45,15 +27,7 @@ function supported_language( $country ) {
 function facebookLinkForCountry( $country ) {
 
     switch ( $country ) {
-        case 'hk':  return "https://www.facebook.com/AVITAHongKong/";
-        case 'sg':  return "https://www.facebook.com/AvitaSG/";
-        case 'tw':  return "https://www.facebook.com/AVITATaiwan/";
-        case 'th':  return 'https://www.facebook.com/AvitaTH/';
-        case 'in':  return 'https://www.facebook.com/AvitaIndia/';
-        case 'cn':
-        case 'ph':
-        case 'vn':
-        case 'id':
+        case 'bd':  return "https://www.facebook.com/AVITAHongKong/";
         default:    return "";
     }
 }
@@ -61,15 +35,7 @@ function facebookLinkForCountry( $country ) {
 function instagramLinkForCountry( $country ) {
 
     switch ( $country ) {
-        case 'hk':  return "https://www.instagram.com/avitahongkong/";
-        case 'sg':  return "https://www.instagram.com/avitasingapore/";
-        case 'tw':  return "https://www.instagram.com/avita_taiwan/";
-        case 'th':  return 'https://www.instagram.com/avitathailand/';
-        case 'in':  return 'https://www.instagram.com/avitaind/';
-        case 'cn':
-        case 'ph':
-        case 'vn':
-        case 'id':
+        case 'bd':  return "https://www.instagram.com/avitahongkong/";
         default:    return "";
     }
 }
@@ -77,15 +43,7 @@ function instagramLinkForCountry( $country ) {
 function twitterLinkForCountry( $country ) {
 
     switch ( $country ) {
-        case 'in':  return 'https://twitter.com/AvitaIndia';
-        case 'hk':
-        case 'sg':
-        case 'tw':
-        case 'th':
-        case 'cn':
-        case 'ph':
-        case 'vn':
-        case 'id':
+        case 'bd':  return 'https://twitter.com/AvitaIndia';
         default:    return "";
     }
 }
@@ -96,26 +54,14 @@ function metaKeywordByCountryAndLanguage( $country, $language ) {
     $keyword = "AVITA LIBER 12.5\", Core i5 Intel CPU, Windows Hello, fingerprint, USB 3.0 ports, USB Type-C, USB-C";
 
     switch ( $country ) {
-        case 'hk':
-            if ( $language == 'tc') {
-                $keyword = "AVITA LIBER 12.5\", Core i5 Intel CPU, Windows Hello, 指紋辨識, USB 3.0連接埠, USB Type-C, USB-C";
+        case 'bd':
+            if ( $language == 'en') {
+                $keyword = "AVITA LIBER 12.5\", Core i5 Intel CPU, Windows Hello, fingerprint, USB 3.0 ports, USB Type-C, USB-C";
             } else {
                 $keyword = "AVITA LIBER 12.5\", Core i5 Intel CPU, Windows Hello, fingerprint, USB 3.0 ports, USB Type-C, USB-C";
             }
             break;
-        case 'cn':
-            $keyword = 'AVITA LIBER 12.5&quot;, Core i5 Intel CPU, Windows Hello, 指纹辨识, USB 3.0连接埠, USB Type-C, USB-C';
-            break;
-        case 'th':
-            $keyword = 'AVITA LIBER 12.5&quot;, Core i5 Intel CPU, Windows Hello, fingerprint, USB 3.0 ports, USB Type-C, USB-C';
-            break;
-
-        case 'id':
-            if ( $language = 'id') {
-                $keyword = 'AVITA LIBER 12.5&quot;, Core i5 Intel CPU, Windows Hello, sidik jari, USB 3.0 ports, USB Type-C, USB-C';
-                break;
-            }
-    }
+         }
 
     return $keyword;
 }
@@ -127,30 +73,15 @@ function metaDescriptionByCountryAndLanguage( $country, $language ) {
 
     switch ( $country ) {
         case 'hk':
-            if ( $language == 'tc') {
-                $description = "LIBER是一趟發掘自我的全新旅程，是藝術、人文與科技的結合體。科技產品不一定只有一種個性，而應該更具靈性。LIBER不止是一台筆記型電腦。加入我們，認識更精彩的自己。";
-            } else {
-                $description = "LIBER is a new journey of self-discovery through art and humanity in technology.  Where the form of a laptop remains, it on top features chic and interchangeable form factors to reveal your true colors.";
-            }
-            break;
-        case 'cn':
-            $description = 'LIBER是一趟发掘自我的全新旅程，是艺术、人文与科技的结合体。科技产品不一定只有一种个性，而应该更具灵性。 LIBER不止是一台笔记型电脑。加入我们，认识更精彩的自己。';
-            break;
-        case 'th':
-
             if ( $language == 'en') {
                 $description = "LIBER is a new journey of self-discovery through art and humanity in technology.  Where the form of a laptop remains, it on top features chic and interchangeable form factors to reveal your true colors.";
             } else {
-                $description = 'LIBER คือการเดินทางแห่งการค้นพบตัวเองแบบใหม่ผ่านศิลปะและมนุษยธรรมในเทคโนโลยี ที่ที่ฟอร์มของแล็ปท็อปยังคงอยู่ แต่เพิ่มความ chic และ ฟอร์มที่ปรับเปลี่ยนได้เพื่อดึงความเป็นคุณออกมาอย่างชัดเจน';
+                $description = 'LIBER is a new journey of self-discovery through art and humanity in technology.  Where the form of a laptop remains, it on top features chic and interchangeable form factors to reveal your true colors.';
             }
+
 
             break;
 
-        case 'id':
-            if ( $language = 'id') {
-                $keyword = 'LIBER adalah sebuah perjalanan mencari jati diri melalui seni dan sisi kemanusiaan didalam teknologi, Dalam bentuk sebuah laptop dengan fitur utama yang menawan dan dapat diganti demi menunjukan siapa sesungguhnya diri kita.';
-                break;
-            }
     }
 
     return $description;
@@ -161,18 +92,8 @@ function metaDescriptionByCountryAndLanguage( $country, $language ) {
 function googleAnalyticCode( $country ) {
 
     switch ( $country ) {
-        case 'id' : return "UA-128035503-8";
-        case 'ph' : return "UA-128035503-9";
-        case 'vn' : return 'UA-128035503-7';
-        case 'cn' : return 'UA-128035503-10';
-        case 'in' : return 'UA-128035503-6';
-        case 'my' : return 'UA-128035503-3';
-        case 'sg' : return 'UA-128035503-5';
-        case 'tw' : return 'UA-128035503-4';
-        case 'th' : return 'UA-128035503-11';
-
-    }
-
+        case 'bd' : return "UA-128035503-8";
+           }
 
     return 'UA-106387992-1';    // Default
 
