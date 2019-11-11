5630c1f3ce4702bc452b3a52604a25dd2660333b
AVITA India <avitaind@gmail.com>
2019-11-11 17:55:25 +0530

    Contact Detals Updated

--- storage/framework/views/8174a967732a42222567e4e0b3f3d762d4d4872e.php
+++ storage/framework/views/8174a967732a42222567e4e0b3f3d762d4d4872e.php
@@ -16,25 +16,24 @@
                         <div class="col-sm-6">
                             <h2 class="contact_us_title">Sales Enquiry :</h2>
 
-                            <div class="mb-2">+230 4633 869</div>
+                            <div class="mb-2">+8809638686868</div>
 
                             <div class="mb-2"><b>Email :</b><br>
-                                info@techstation.mu</div>
+                                </div>
 
                             <div><b><?php echo app('translator')->getFromJson('site.contactus_operating'); ?> :</b><br>
-                                Monday to Friday : 8:30 to 17:00</div>
+                                Saturday to Thursday : From 8 am to 8 pm</div>
 
 
                             <div class="mb-2"><b>Address :</b><br>
-                                TechStation Ltd<br/> 7, Avenue Des Orchidées, 72238 Quatre-Bornes</div>
+                                B-Trac Technologies Ltd. <br/>House 68, Road 11, Block H, Banani, Dhaka-1213, Bangladesh.</div>
                          <!---
                           <h2 class="contact_us_title">For Service :</h2>
                             <div class="mb-2">Toll Free : 1800-22-3902</div>
                           -------->
                         </div>
                         <div class="col-sm-6">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3742.88146815096!2d57.48051591494494!3d-20.263750054007247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x217c5b40882cf0ab%3A0x48041307c14d6ae1!2s7+Ave+Des+Orchidees%2C+Quatre+Bornes%2C+Mauritius!5e0!3m2!1sen!2sin!4v1561119288837!5m2!1sen!2sin" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>                        </div>
-                    </div>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.8034804168697!2d90.40677941498213!3d23.790011384569848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c6494f45b1b1%3A0xde65ab465933a881!2sBangla%20Trac%20Communications%20Limited!5e0!3m2!1sen!2sin!4v1573475065236!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>                    </div>
                 </div>
 
 
