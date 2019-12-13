43eb656eb91304761c3b8872d27f9e81ccaa1869
avitaind <52917489+avitaind@users.noreply.github.com>
2019-12-13 23:50:53 +0530

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -26,9 +26,11 @@
                                         <li class="nav-item">
                                             <a class="nav-link px-md-4 py-2" href="{{ route('product.overview', ['admiror']) }}">@lang('site.admiror')</a>
                                         </li>
+                                        <!--
                                         <li class="nav-item">
                                             <a class="nav-link px-md-4 py-2" href="{{ route('product.overview', ['liber12']) }}">@lang('site.liber12')</a>
                                         </li>
+                                        --->
                                         <li class="nav-item">
                                                 <a class="nav-link px-md-4 py-2" href="{{ route('product.overview', ['liber']) }}">@lang('site.liber')</a>
                                             </li>
