b91b65a0ab09d16f71aabbde691d123d577ecb5e
avitaind <52917489+avitaind@users.noreply.github.com>
2019-12-14 22:38:27 +0530

    Update helper.php

--- app/helper.php
+++ app/helper.php
@@ -68,18 +68,7 @@ function metaKeywordByCountryAndLanguage( $country, $language ) {
                 $keyword = "AVITA LIBER 12.5\", Core i5 Intel CPU, Windows Hello, fingerprint, USB 3.0 ports, USB Type-C, USB-C";
             }
             break;
-        case 'cn':
-            $keyword = 'AVITA LIBER 12.5&quot;, Core i5 Intel CPU, Windows Hello, 指纹辨识, USB 3.0连接埠, USB Type-C, USB-C';
-            break;
-        case 'th':
-            $keyword = 'AVITA LIBER 12.5&quot;, Core i5 Intel CPU, Windows Hello, fingerprint, USB 3.0 ports, USB Type-C, USB-C';
-            break;
-
-        case 'id':
-            if ( $language = 'id') {
-                $keyword = 'AVITA LIBER 12.5&quot;, Core i5 Intel CPU, Windows Hello, sidik jari, USB 3.0 ports, USB Type-C, USB-C';
-                break;
-            }
+ 
     }
 
     return $keyword;
@@ -98,24 +87,7 @@ function metaDescriptionByCountryAndLanguage( $country, $language ) {
                 $description = "LIBER is a new journey of self-discovery through art and humanity in technology.  Where the form of a laptop remains, it on top features chic and interchangeable form factors to reveal your true colors.";
             }
             break;
-        case 'cn':
-            $description = 'LIBER是一趟发掘自我的全新旅程，是艺术、人文与科技的结合体。科技产品不一定只有一种个性，而应该更具灵性。 LIBER不止是一台笔记型电脑。加入我们，认识更精彩的自己。';
-            break;
-        case 'th':
-
-            if ( $language == 'en') {
-                $description = "LIBER is a new journey of self-discovery through art and humanity in technology.  Where the form of a laptop remains, it on top features chic and interchangeable form factors to reveal your true colors.";
-            } else {
-                $description = 'LIBER คือการเดินทางแห่งการค้นพบตัวเองแบบใหม่ผ่านศิลปะและมนุษยธรรมในเทคโนโลยี ที่ที่ฟอร์มของแล็ปท็อปยังคงอยู่ แต่เพิ่มความ chic และ ฟอร์มที่ปรับเปลี่ยนได้เพื่อดึงความเป็นคุณออกมาอย่างชัดเจน';
-            }
-
-            break;
-
-        case 'id':
-            if ( $language = 'id') {
-                $keyword = 'LIBER adalah sebuah perjalanan mencari jati diri melalui seni dan sisi kemanusiaan didalam teknologi, Dalam bentuk sebuah laptop dengan fitur utama yang menawan dan dapat diganti demi menunjukan siapa sesungguhnya diri kita.';
-                break;
-            }
+    
     }
 
     return $description;
@@ -126,18 +98,18 @@ function metaDescriptionByCountryAndLanguage( $country, $language ) {
 function googleAnalyticCode( $country ) {
 
     switch ( $country ) {
-        case 'bd' : return "UA-128035503-8";
+        case 'bd' : return "UA-154623067-1";
 
     }
 
 
-    return 'UA-106387992-1';    // Default
+    return 'UA-154623067-1';    // Default
 
 
 }
 function GoogleMap( $key ) {
 
-    return 'AIzaSyBcUXTGsX48sxYZ_J91C_oWgvcv1oNFWEQ';    // Default
+    return 'AIzaSyBWQCKIl7B4w27KcSW-tW4ja_Rk4SbcPnk';    // Default
 
 }
 function BaiduMap( $key ) {
