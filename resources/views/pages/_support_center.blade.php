4d647f0613b237f344e6b1385aae1c016668d5e7
root <root@ip-172-31-43-173.us-east-2.compute.internal>
2019-12-19 12:14:43 +0000

    Support email hide

--- resources/views/pages/_support_center.blade.php
+++ resources/views/pages/_support_center.blade.php
@@ -21,10 +21,10 @@
                 {{--<div class="col-12 col-sm-8 col-md-8 col-lg-9">{{ $center->fax }}</div>--}}
             {{--</div>--}}
 
-            <div class="row my-2 my-sm-1 ls-0">
+          {{--- <div class="row my-2 my-sm-1 ls-0"> ---}}
                 <div class="col-12 col-sm-4 col-md-4 col-lg-3">@lang('site.form_mail')</div>
                 <div class="col-12 col-sm-8 col-md-8 col-lg-9"><a href="mailto:{{ $center->email }}">{{ $center->email }}</a></div>
-            </div>
+          {{---  </div> ---}}
         </div>
  <div class="col-12 col-md-5">
         <div class="map-wrap embed-responsive embed-responsive-16by9">
