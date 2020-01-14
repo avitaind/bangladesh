e16e40019856b2845ffc1a1fe0b59c65ba5a41bd
AVITA India <avitaind@gmail.com>
2020-01-14 17:47:39 +0530

    Banner images reduced

--- resources/views/banner_hk.blade.php
+++ resources/views/banner_hk.blade.php
@@ -51,59 +51,13 @@
                 </div>
             </div>
          
-        <!---
-        <div class="carousel-item">
-                <div class="responsive-block">
-                    <div class="banner-block responsive-item">
-                        @if( App::isLocale('en') )
-                            <div class="banner-bg hidden-sm-down"
-                                 style="background-image: url('/images/banner/magus-web.jpg')"></div>
-                        @else
-                            <div class="banner-bg hidden-sm-down"
-                                 style="background-image: url('/images/banner/magus-web.jpg')"></div>
-                        @endif
-                        <div class="banner-bg hidden-md-up"
-                             style="background-image: url('/images/banner/magus-mob.jpg')"></div>
-                    </div>
-                </div>
-            </div>
-            --->
-         <!--   <div class="carousel-item ">
-                <div class="responsive-block">
-                    <div class="banner-block responsive-item  d-flex align-items-center">
-                        <video class="leadin-video video-bgv1" muted autoplay playsinline>
-                            <source src="/videos/bgv1.mp4" type="video/mp4">
-                        </video>
-                        <div class="banner-info mb-5 pb-5 w-100 align-self-end">
-                            <div class="btn-group mt-5 pt-5 pt-md-0 ls-0">
-                                <a class="btn btn-more mx-auto mt-5 mt-md-0 font-weight-normal" href="{{ route('product.overview', 'liber') }}">@lang('site.home_learnmore')<i class="fa fa-chevron-right ml-2" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="carousel-item">
-                <div class="responsive-block">
-                    <div class="banner-block responsive-item d-flex align-items-center">
-                        <video class="leadin-video video-bgv2" muted playsinline>
-                            <source src="/videos/bgv2.mp4" type="video/mp4">
-                        </video>
-                        <div class="banner-info mb-5 pb-5 w-100 align-self-end">
-                            <div class="btn-group mt-5 pt-5 pt-md-0 ls-0">
-                                <a class="btn btn-more mx-auto mt-5 mt-md-0 font-weight-normal" href="{{ route('product.overview', 'liber') }}">@lang('site.home_learnmore')<i class="fa fa-chevron-right ml-2" aria-hidden="true"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            --->
+   
 
         </div>
         <ol class="carousel-indicators">
-            <li data-target="#home_banner" data-slide-to="0" class="active"></li>
+          <li data-target="#home_banner" data-slide-to="0" class="active"></li>
           <li data-target="#home_banner" data-slide-to="1"></li>
-            <li data-target="#home_banner" data-slide-to="2"></li>
+          <li data-target="#home_banner" data-slide-to="2"></li>
         </ol>
     </div>
 </section>
@@ -127,10 +81,7 @@
 @section('js')
     <script type="text/javascript">
         var imageTimer = null;
-        /*   $('video').on('ended', function(){
-               $('#home_banner').carousel('next');
-           });
-       */
+      
         $('#home_banner').on('init slide.bs.carousel', function (e) {
             clearTimeout( imageTimer );
             imageTimer = setTimeout( function( ){
