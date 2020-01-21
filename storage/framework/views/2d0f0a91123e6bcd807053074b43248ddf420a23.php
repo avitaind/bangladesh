ff8bbc8d9b49c4f9a1d7be8db04cfe91878c846f
AVITA India <avitaind@gmail.com>
2020-01-21 12:52:15 +0530

    Receiving newsletter text removed

--- storage/framework/views/2d0f0a91123e6bcd807053074b43248ddf420a23.php
+++ storage/framework/views/2d0f0a91123e6bcd807053074b43248ddf420a23.php
@@ -57,10 +57,10 @@
 
   <section class="email-subscription py-5 ls-0">
    <form class="email-subscription-form col-12 col-lg-8 col-xl-6 mx-auto">
-            <h2 class="text-center mt-4 mb-3 font-weight-light"><?php echo app('translator')->getFromJson('site.home_join'); ?></h2>
+         <!--     <h2 class="text-center mt-4 mb-3 font-weight-light"><?php echo app('translator')->getFromJson('site.home_join'); ?></h2>
             <div class="text-center mt-3 mb-4 lead"><?php echo app('translator')->getFromJson('site.home_receiving'); ?></div>
             <div class="row my-4 no-gutters justify-content-center">
-         <!--     <div class="col-12 col-sm-7 col-md-6 col-lg-7 ml-auto">
+            <div class="col-12 col-sm-7 col-md-6 col-lg-7 ml-auto">
                     <div class="input-group">
                         <div class="input-group-addon overlay-addon"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
                         <input type="email" name="subscription_email" class="form-control" required>
