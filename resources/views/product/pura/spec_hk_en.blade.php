27cf3fcfbdb1398c6ca9c1122e7207c825a9620c
AVITA India <avitaind@gmail.com>
2020-02-06 19:40:17 +0530

    Under Test Condition text added

--- resources/views/product/pura/spec_hk_en.blade.php
+++ resources/views/product/pura/spec_hk_en.blade.php
@@ -182,9 +182,9 @@
                     <li>Colors of actual products may differ from product shots due to photography lighting or display setting of your viewing device.</li>
                     <li>We try our best to provide accurate and complete product information online yet we reserve the rights to keep, change or correct any information without further notice.</li>
                     <li>Windows is either registered trademark or trademark of Microsoft Corporation in the United States and/or other countries.</li>
+                    <li>*Under Test Conditions</li>
                      
                 </ul>
-                <span><small style="float:right;">*Under Test Conditions</small></span><br/>
             </div>
         </section>
 
