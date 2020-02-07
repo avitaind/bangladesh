6ff975d7bf0565cf31032d13936237f2491183ee
AVITA India <avitaind@gmail.com>
2020-02-07 18:08:07 +0530

    Custom Controller Edited

--- app/Http/Controllers/CustomController.php
+++ app/Http/Controllers/CustomController.php
@@ -5,17 +5,68 @@ use Illuminate\Support\Facades\Mail;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+
 use App\Campus;
 
 class CustomController extends Controller
 {
 
-    public function index(){
+    public function chirstmasOffer()
+    {
+        return view('chirstmas-offer');
+    }
+
+    public function campus(){
         return view('campus');
     }
 
     public function store(Request $request)
     {
-        console.log("Hello");
-    }
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone'=>'required',
+            'internship' => 'required',
+            'college' => 'required',
+            'fest'=>'required',
+            'position'=>'required'
+        ]);
+     
+      /*  Campus::create($request->all());
+
+        \Mail::send('emails.campus',
+            array(
+                'name' => $request->get('name'),
+                'email' => $request->get('email'),
+                'phone' => $request->get('phone'),
+                'internship' => $request->get('internship'),
+                'college' => $request->get('college'),
+                'fest' => $request->get('fest'),
+                'position' => $request->get('position'),
+
+            ), function ($message) use ($request)
+            {
+                $message->from('avitaind@gmail.com');
+                $message->to('avitaind@gmail.com', 'Admin')->subject('Campus Ambassador Program');
+    });
+
+	    return redirect()->back()->with('message', 'Thank you for your submission . You shall receive a confirmation mail shortly');
+       
+
+}*/
+$campus = new Campus([
+    'name' => $request->get('name'),
+    'email' => $request->get('email'),
+    'phone' => $request->get('phone'),
+    'internship' => $request->get('internship'),
+    'college' => $request->get('college'),
+    'fest' => $request->get('fest'),
+    'position' => $request->get('position'),
+
+]);
+$campus->save();
+return redirect()->back()->with('message', 'Thank you for your submission. You shall receive a confirmation mail shortly!');
+
+}
+
 }
