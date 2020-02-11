4e7feca2c055f4f11cb7408863c009de180682e7
AVITA India <avitaind@gmail.com>
2020-02-11 13:17:11 +0530

    CAP email text changed

--- resources/views/emails/campus.blade.php
+++ resources/views/emails/campus.blade.php
@@ -1,2 +1,2 @@
 <h2>Dear {{ $name }},</h2>
-<img src="{{ $message->embed(public_path() . '/images/cap-auto-reply.png') }}" alt="" />
+<!-- <img src="{{ $message->embed(public_path() . '/images/cap-auto-reply.png') }}" alt="" /> -->
