ab5d794dc7e91fcd2f64bcfb7c779be4916e6785
avitaind <52917489+avitaind@users.noreply.github.com>
2020-03-19 17:08:55 +0530

    Update support.blade.php

--- resources/views/pages/support.blade.php
+++ resources/views/pages/support.blade.php
@@ -59,8 +59,10 @@
                             </form>
 
                               <br/>
-                            <div class="mb-2"><b>Email :</b> bd_support@avita.com </div>
-                            <div class="mb-2"><b>General Query :</b> +880-9638686868 </div>
+                            <div class="mb-2"><b>Email:</b> bd_support@avita.com </div>
+                            <div class="mb-2"><b>General Query:</b> +880-9638686868 </div>
+                             <div class="mb-2"><b>Operation Time:</b> Sat-Thu: 08:00 -22:00 Close on Fri & Public Holiday </div>
+
 
                         </div>
                     </div>
