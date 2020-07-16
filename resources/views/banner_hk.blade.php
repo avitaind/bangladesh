4b6678cb28115605ff981a8cf74df04fbe50a081
avitaind <52917489+avitaind@users.noreply.github.com>
2020-07-16 15:45:07 +0530

    Update banner_hk.blade.php

--- resources/views/banner_hk.blade.php
+++ resources/views/banner_hk.blade.php
@@ -99,6 +99,7 @@
           <li data-target="#home_banner" data-slide-to="1"></li>
           <li data-target="#home_banner" data-slide-to="2"></li>
           <li data-target="#home_banner" data-slide-to="3"></li>
+          <li data-target="#home_banner" data-slide-to="4"></li>
 
         </ol>
     </div>
