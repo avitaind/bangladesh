822fce382633bf3080b463a389a5eb32e08b0d8c
AVITA India <avitaind@gmail.com>
2020-07-31 10:10:58 +0530

    avita bangladesh banner removed

--- resources/views/banner_hk.blade.php
+++ resources/views/banner_hk.blade.php
@@ -18,26 +18,7 @@
                 </div>
             </div>
             
-                <div class="carousel-item">
-                <div class="responsive-block">
-                  
-                    <div class="banner-block responsive-item">
-                        @if( App::isLocale('en') )
-                            <div class="banner-bg hidden-sm-down"
-                                 style="background-image: url('/images/banner/avita-bd-web-banner.jpg')"></div>
-                        @else
-                            <div class="banner-bg hidden-sm-down"
-                                 style="background-image: url('/images/banner/avita-bd-web-banner.jpg')"></div>
-                        @endif
-                        <div class="banner-bg hidden-md-up"
-                             style="background-image: url('/images/banner/avita-bd-mob-banner.jpg')"></div>
-                    </div>
-                
-                </div>
-            </div>
-            
-            
-                <div class="carousel-item">
+         <div class="carousel-item">
                 <div class="responsive-block">
                     <a href="{{ route('product.overview', ['pura']) }}">
                     <div class="banner-block responsive-item">
@@ -117,9 +98,6 @@
           <li data-target="#home_banner" data-slide-to="2"></li>
           <li data-target="#home_banner" data-slide-to="3"></li>
           <li data-target="#home_banner" data-slide-to="4"></li>
-          <li data-target="#home_banner" data-slide-to="5"></li>
-
-
         </ol>
     </div>
 </section>
