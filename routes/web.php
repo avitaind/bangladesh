cc8d1960a85ff5a1fdd84a012be02febf62c1b99
AVITA India <avitaind@gmail.com>
2020-10-28 15:52:14 +0530

    google jquery added v11

--- routes/web.php
+++ routes/web.php
@@ -107,10 +107,12 @@ if ( Request::segment(1) != 'admin') {
         } )->name('products');
 
         Route::get('/product/{slug}', 'ProductsController@showProductFeatures')->name('product.overview');
+        Route::get('/product/{slug}/spec_new', 'ProductsController@showProductSpecNew')->name('product.spec_new');
         Route::get('/product/{slug}/spec', 'ProductsController@showProductSpec')->name('product.spec');
         Route::get('/product/{slug}/support', 'ProductsController@showProductSupport')->name('product.support');
-      //  Route::get('/product/{slug}/where_to_buy', 'ProductsController@whereToBuy')->name('product.map');
-      Route::get('/where-to-buy', 'ProductsController@whereToBuy')->name('product.map');
+       // Route::get('/product/{slug}/where_to_buy', 'ProductsController@whereToBuy')->name('product.map');
+
+     Route::get('where-to-buy', 'ProductsController@whereToBuy');
   
       
     //});
